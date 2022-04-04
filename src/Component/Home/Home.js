79155631f0f2053d057fc31fba1651f34e5a6257
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-04-04 17:41:46 +0600

    _redirects file added

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import Image from "../Assets/Images/camera.png";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Wellcome to here</h1>
+  return (
+    <div>
+      <section>
+        <div className="first_half">
+          <h1>Your Next Camera</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi error
+            cum repellat impedit ipsa exercitationem. Quaerat totam
+            exercitationem facere dolor.
+          </p>
         </div>
-    );
+        <div className="half">
+          <img src={Image} alt="img" />
+        </div>
+      </section>
+
+      <div className="review_container">
+        <h1>Review Area </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
