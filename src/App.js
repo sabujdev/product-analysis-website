0ade73ebb6104d7a9ac54914b88e967a456dfbe7
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-04-04 16:35:26 +0600

    all component or Route Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Component/Home/Home';
-import Navbar from './Component/Navbar/Navbar';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Component/Home/Home";
+import Navbar from "./Component/Navbar/Navbar";
+import Reviews from "./Component/Reviews/Reviews";
+import Dashboard from "./Component/Dashboard/Dashboard";
+import Blogs from "./Component/Blogs/Blogs";
+import About from "./Component/About/About";
+import NotFound from "./Component/NotFound/NotFound";
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<Home></Home>}>Home</Route>
-        <Route path='/'>Home</Route>
-        <Route path='/'>Home</Route>
-        <Route path='/'>Home</Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
-    
     </div>
   );
 }
---
+++ src/Component/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reviews = () => {
+  return (
+    <div>
+      <h1>What Your customer say!</h1>
+    </div>
+  );
+};
+
+export default Reviews;
